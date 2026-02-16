19a4769a7e510ed6fa7158b39ef5db2226dbcd3e
Quinn Unger <quinn.unger@pm.me>
2026-02-16 18:57:18 +0000

    use quartus builder supplied by moppkgs

--- flake.nix
+++ flake.nix
@@ -1,35 +1,58 @@
 {
     inputs = {
         nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+        moppkgs.url = "github:Mop-u/moppkgs";
     };
 
-    outputs = { self, nixpkgs, flake-utils, ... }:
-    let
-        system = "x86_64-linux";
+    outputs =
+        {
+            self,
+            nixpkgs,
+            moppkgs,
+            flake-utils,
+            ...
+        }:
+        let
+            system = "x86_64-linux";
 
-        pkgs = import nixpkgs {
-            inherit system;
-            config.allowUnfree = true;
+            pkgs =
+                (import nixpkgs {
+                    inherit system;
+                    config.allowUnfree = true;
+                }).extend
+                    moppkgs.overlays.quartus-prime-pro;
+
+            mkVersion =
+                {
+                    version,
+                    edition,
+                    extraArgs ? null,
+                }:
+                let
+                    quartusSource = (import ./sources.nix { })."v${builtins.toString version}".${edition};
+                in
+                pkgs.quartus-prime-pro.override (
+                    {
+                        inherit quartusSource;
+                    }
+                    // (
+                        if extraArgs != null then
+                            { quartusOptions = extraArgs; }
+                        else
+                            {
+                                quartusOptions = {
+                                    devices = quartusSource.defaultDevices;
+                                    installs = quartusSource.defaultInstalls;
+                                };
+                            }
+                    )
+                );
+
+        in
+        {
+
+            nixosModules.quartus = import ./module.nix { inherit mkVersion; };
+
+            packages.${system} = { inherit mkVersion; };
         };
-
-        sources = import ./sources.nix {};
-
-        mkQuartusUnwrapped = pkgs.callPackage ./make-quartus-unwrapped.nix {
-            inherit (pkgs.stdenv) mkDerivation;
-        };
-
-        mkQuartus = pkgs.callPackage ./make-quartus.nix {
-            inherit mkQuartusUnwrapped;
-        };
-
-        mkVersion = { version, edition, extraArgs }: let
-            source = sources."v${builtins.toString version}".${edition};
-        in mkQuartus { inherit source extraArgs; };
-        
-    in {
-
-        nixosModules.quartus = import ./module.nix {inherit mkVersion;};
-
-        packages.${system} = {inherit mkVersion;};
-    };
 }
